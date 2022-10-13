11f9395c6acb62f9aaa5e428f66c6227236174fe
Megumi Kimura <kimuramegumi@gmail.com>
2022-10-13 23:19:11 +0200

    add search btn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import { menuBtnFunc } from "./utils/menuBtn.js";
 
 const search = document.querySelector("#search");
+const searchBtn = document.querySelector("#search-button");
 // const menuBtn = document.querySelector(".menu-btn");
 
 // menuBtn.addEventListener("click", menuBtnFunc);
@@ -8,4 +9,7 @@ search.addEventListener("keypress", function (event) {
   if (event.keyCode === 13) {
     window.location.href = `/search-results.html?search=${this.value}`;
   }
+});
+searchBtn.addEventListener("click", function (event) {
+  window.location.href = `/search-results.html?search=${search.value}`;
 });
