c7cc48c04cd912f8b215ba0f3dd264c47b51b63f
Megumi Kimura <kimuramegumi@gmail.com>
2022-10-17 21:39:03 +0200

    fix layout

--- js/utils/toggleDisplay.js
+++ js/utils/toggleDisplay.js
@@ -1,10 +1,13 @@
 let displayOpen = false;
 
 export function toggleDisplay(event, target) {
+  event.target.classList.toggle("fa-circle-plus");
+  event.target.classList.toggle("fa-circle-minus");
   if (!displayOpen) {
     event.target.classList.add(".show");
     displayOpen = true;
     target.style.display = "flex";
+    console.log(event.target);
   } else {
     event.target.classList.remove("show");
     displayOpen = false;
