64d7e2724bfba3251511656fb87f700f7d5b39d3
Megumi Kimura <kimuramegumi@gmail.com>
2023-01-17 11:27:15 +0100

    add test.yml

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ require("dotenv/config");
 
 function test(expectedSecret) {
   const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
-
+  console.log(`The Secret is ${process.env.MEANING_OF_LIFE}`);
   console.log(`The Secret does${secretMatches ? "" : " not"} match`);
   return secretMatches;
 }
